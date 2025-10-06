d67b6139b2482059d9524a28bc98f1afb65bbaa8
Ahmadfarhan67 <123234895+Ahmadfarhan67@users.noreply.github.com>
2025-10-06 05:09:31 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
     const whitePageURL = "https://www.ikea.com/at/en/p/gulligast-baby-gym-multicolour-90484258/";
-    const blackPageURL = "https://uplevelrewarded.com/aff_c?offer_id=1232&aff_id=45076";
+    const blackPageURL = "https://uplevelrewarded.com/aff_c?offer_id=1232&aff_id=45076&source=scale";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -25,6 +25,7 @@ export default function handler(req, res) {
   
     res.end();
   }
+
 
 
 
